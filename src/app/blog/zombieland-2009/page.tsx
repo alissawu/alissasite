834e0591af62d63b4b9a1f17c38e6a36c0bdbfed
Alissa <alissawu77@gmail.com>
2025-10-15 01:10:17 -0400

    blog

--- src/app/blog/zombieland-2009/page.tsx
+++ src/app/blog/zombieland-2009/page.tsx
@@ -56,6 +56,7 @@ export default function Poast() {
                 Not because I&apos;m Puritan or anything, it just didn&apos;t feel like they established the emotional build-up enough.
                 Can movie directors STOP making movies with a male MC, and putting a woman in the &quot;main character group&quot;, and automatically
                 assuming the presence of a man and a woman is enough to imply/establish a romantic subtext? ugh!
+                you guys are all lazy and don't know how to write real emotional connection and this is why we have hookup culture and evil men!!!
             </p>
             <p>
                 PS: the tiktoks
